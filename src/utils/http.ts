4d6ada96094ce60f23792741c94168e3911def95
wa1kman <wa1kman@163.com>
2023-09-14 13:48:55 +0800

    feat: 请求拦截器封装

---
+++ src/utils/http.ts
@@ -0,0 +1,30 @@
+import { useMemberStore } from '@/stores'
+
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+// 添加拦截器
+const httpInterceptor = {
+  invoke(options: UniApp.RequestOptions) {
+    // 1. 非http开头需拼接的地址
+    if (!options.url.startsWith('http')) {
+      options.url = baseURL + options.url
+    }
+    // 2.请求超时
+    options.timeout = 10000
+    // 3.请求头
+    options.header = {
+      ...options.header,
+      'source-client': 'miniapp',
+    }
+    // 添加token
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
+    if (token) {
+      options.header.Authorization = token
+    }
+    console.log(options)
+  },
+}
+
+uni.addInterceptor('request', httpInterceptor)
+uni.addInterceptor('uploadFile', httpInterceptor)
