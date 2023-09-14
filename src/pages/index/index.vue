38c383b1c86c9e77f2b241f4a382596ef35bb695
wa1kman <wa1kman@163.com>
2023-09-14 16:14:54 +0800

    feat: 轮播图数据

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home'
+import { onLoad } from '@dcloudio/uni-app'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { ref } from 'vue'
+
+const bannerList = ref([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  console.log(res)
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
---
+++ src/services/home.ts
@@ -0,0 +1,12 @@
+import { http } from '@/utils/http'
+
+// 首页广告区域
+export const getHomeBannerAPI = (distributionSite = 1) => {
+  return http({
+    method: 'GET',
+    url: '/home/banner',
+    data: {
+      distributionSite,
+    },
+  })
+}
